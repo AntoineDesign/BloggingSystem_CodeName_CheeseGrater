eb741aa1520e94e73e7929534a5958b6682ecf1f
AntoineDesign <maplemusic1@gmail.com>
2014-12-14 16:37:46 -0500

    404 Finished and style finished too!
    Well got the entire style sheet and 404 not found page done!

--- BlogSystem/app/views/404.html.php
+++ BlogSystem/app/views/404.html.php
@@ -4,4 +4,17 @@
 	<meta charset="utf-8" />
 
 	<title>404 NOT FOUND |	<?php echo config('blog.title') ?></title>
-	<link href="<?php echo site_url() ?>"
+	<link href="<?php echo site_url() ?>assets/css/style.css" rel="stylesheet" />
+
+	<!-- Include open sans foonntttttt (really amazing font ya know?) -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700&subset=latin,cyrillic-ext" rel="stylesheet" />
+</head>
+
+<body>
+
+	<div class="center message">
+		<h1>This page doesnt exsit what did you do?</h1>
+		<p>Want to go back to the <a href="<?php echo site_url() ?>">Hompage</a></p>
+	</div>
+</body>
+</html>
